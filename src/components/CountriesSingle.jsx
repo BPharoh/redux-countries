106c37dac1010d2800baf87e7f804c83e95d77e4
Otulugbu Alexander <s2200742@Alexander-MacBook-Air.local>
2023-02-10 12:12:32 +0200

    Added open weather api call

--- src/components/CountriesSingle.jsx
+++ src/components/CountriesSingle.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 import { Container } from 'react-bootstrap';
 
 const CountriesSingle = () => {
+  const [weather, setWeather] = useState('');
+  const [error, setError] = useState('');
 
+  const countryTest =  {
+    capital: "Helsinki"
+  }
+
+  useEffect(()  => {
+    axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${countryTest.capital}&units=metric&appid=${process.env.REACT_APP_OPENWEATHER_KEY}`
+      ).catch((error) => {
+      console.log(error);
+      setError(true)
+    })
+    .then((res) => {
+      setWeather(res.data);
+    })
+  },[countryTest.capital]  )
+
+  console.log('weather', weather);
 
   return (
     <Container>
